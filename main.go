9ec96df38a776ad4700ca7ce4b7af526ca37de7c
Ethan <ethanvolk2424@gmail.com>
2024-11-30 19:56:31 -0600

    -

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	day01 "AOC24/programs/day01"
 	"fmt"
 )
 
@@ -11,5 +12,14 @@ import (
 
 func main() {
 	fmt.Println("Starting The AdventOfCode24 Script")
+
+	// CHOOSE SETTINGS
+	m := 1 // 0 = Test Input | 1 = Actual Input
+
+	day1aans, day1atime := day01.Day1a(m) //Complete
+	day1bans, day1btime := day01.Day1b(m) //Complete
+
+	fmt.Printf("Day 01a | Answer: %-11d | Taking: %s\n", day1aans, day1atime)
+	fmt.Printf("Day 01b | Answer: %-11d | Taking: %s\n", day1bans, day1btime)
 
 }
