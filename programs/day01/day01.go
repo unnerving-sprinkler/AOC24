caa2cd0b3a948efd7b0ff67de0a35d1e29873353
Ethan <ethanvolk2424@gmail.com>
2024-11-30 23:34:12 -0600

    Day1 Working Complete

--- programs/day01/day01.go
+++ programs/day01/day01.go
@@ -3,40 +3,78 @@
 package day01
 
 import (
-	//util "AOC23/programs"
+	util "AOC24/programs"
+	"math"
+	"sort"
 	"time"
 )
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 01A ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 func Day1a(m int) (int, time.Duration) {
 	start := time.Now()
-	//var lines []string
+	var lines []string
 	if m == 0 {
-		//lines = util.Returnlines("inputdata/day_01/day_01_test.txt")
+		lines = util.Returnlines("inputdata/day_01/day_01_test.txt")
 	} else {
-		//lines = util.Returnlines("inputdata/day_01/day_01_actual.txt")
+		lines = util.Returnlines("inputdata/day_01/day_01_actual.txt")
 	}
 
 	//Setup VARS For Today
+	var firstnumbers []int
+	var secondnumbers []int
+	var score float64
 
 	//Days Program
+	for i := 0; i < len(lines); i++ {
+		splitline := util.Splitlines(lines[i], " ")
+		firstnumbers = append(firstnumbers, util.StringtoNumber(splitline[0]))
+		secondnumbers = append(secondnumbers, util.StringtoNumber(splitline[1]))
+	}
 
-	return 0, time.Since(start)
+	sort.Ints(firstnumbers)
+	sort.Ints(secondnumbers)
+
+	for i := 0; i < len(firstnumbers); i++ {
+		score += math.Abs(float64(firstnumbers[i]) - float64(secondnumbers[i]))
+	}
+
+	return int(score), time.Since(start)
 }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 01B ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 func Day1b(m int) (int, time.Duration) {
 	start := time.Now()
-	//var lines []string
+	var lines []string
 	if m == 0 {
-		//lines = util.Returnlines("inputdata/day_01/day_01_test.txt")
+		lines = util.Returnlines("inputdata/day_01/day_01_test.txt")
 	} else {
-		//lines = util.Returnlines("inputdata/day_01/day_01_actual.txt")
+		lines = util.Returnlines("inputdata/day_01/day_01_actual.txt")
 	}
 
 	//Setup VARS For Today
+	var firstnumbers []int
+	var secondnumbers []int
+	var score int
 
 	//Days Program
+	for i := 0; i < len(lines); i++ {
+		splitline := util.Splitlines(lines[i], " ")
+		firstnumbers = append(firstnumbers, util.StringtoNumber(splitline[0]))
+		secondnumbers = append(secondnumbers, util.StringtoNumber(splitline[1]))
+	}
 
-	return 0, time.Since(start)
+	sort.Ints(firstnumbers)
+	sort.Ints(secondnumbers)
+
+	for i := 0; i < len(firstnumbers); i++ {
+		occurances := 0
+		for j := 0; j < len(secondnumbers); j++ {
+			if firstnumbers[i] == secondnumbers[j] {
+				occurances++
+			}
+		}
+		score += firstnumbers[i] * occurances
+	}
+
+	return score, time.Since(start)
 }
--- programs/util.go
+++ programs/util.go
@@ -3,6 +3,8 @@ package until
 import (
 	"bufio"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func Returnlines(filepath string) []string {
@@ -15,4 +17,20 @@ func Returnlines(filepath string) []string {
 		lines = append(lines, scanner.Text()) //Append Each Line Into An Array
 	}
 	return lines
+}
+
+func Splitlines(line string, deliminator string) []string {
+	eachcharstring := (strings.Split(line, deliminator))
+	var cleanCharString []string
+	for i := 0; i < len(eachcharstring); i++ {
+		if eachcharstring[i] != "" {
+			cleanCharString = append(cleanCharString, eachcharstring[i])
+		}
+	}
+	return cleanCharString
+}
+
+func StringtoNumber(str string) int {
+	stringasint, _ := strconv.Atoi(str)
+	return stringasint
 }
