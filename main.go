39d0c2836e91b89d316e4e08b0405af461c864e4
Ethan <ethanvolk2424@gmail.com>
2024-12-03 19:39:43 -0600

    setup to start day03

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	day01 "AOC24/programs/day01"
 	day02 "AOC24/programs/day02"
+	day03 "AOC24/programs/day03"
 	"fmt"
 )
 
@@ -21,11 +22,16 @@ func main() {
 	day1bans, day1btime := day01.Day1b(m) //Complete
 	day2aans, day2atime := day02.Day2a(m) //Complete
 	day2bans, day2btime := day02.Day2b(m) //Complete
+	day3aans, day3atime := day03.Day3a(m) //Complete
+	day3bans, day3btime := day03.Day3b(m) //Complete
 
 	fmt.Printf("Day 01a | Answer: %-11d | Taking: %s\n", day1aans, day1atime)
 	fmt.Printf("Day 01b | Answer: %-11d | Taking: %s\n", day1bans, day1btime)
 
 	fmt.Printf("Day 02a | Answer: %-11d | Taking: %s\n", day2aans, day2atime)
-	fmt.Printf("Day 01b | Answer: %-11d | Taking: %s\n", day2bans, day2btime)
+	fmt.Printf("Day 02b | Answer: %-11d | Taking: %s\n", day2bans, day2btime)
+
+	fmt.Printf("Day 03a | Answer: %-11d | Taking: %s\n", day3aans, day3atime)
+	fmt.Printf("Day 03b | Answer: %-11d | Taking: %s\n", day3bans, day3btime)
 
 }
---
+++ programs/day03/day03.go
@@ -0,0 +1,41 @@
+// /programs/day03.go
+
+package day03
+
+import (
+	"time"
+)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 01A ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+func Day2a(m int) (int, time.Duration) {
+	start := time.Now()
+	//var lines []string
+	if m == 0 {
+		//lines = util.Returnlines("inputdata/day_03/day_03_test.txt")
+	} else {
+		//lines = util.Returnlines("inputdata/day_03/day_03_actual.txt")
+	}
+
+	//Setup VARS For Today
+
+	//Days Program
+
+	return 0, time.Since(start)
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 01B ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+func Day2b(m int) (int, time.Duration) {
+	start := time.Now()
+	//var lines []string
+	if m == 0 {
+		//lines = util.Returnlines("inputdata/day_03/day_03_test.txt")
+	} else {
+		//lines = util.Returnlines("inputdata/day_03/day_03_actual.txt")
+	}
+
+	//Setup VARS For Today
+
+	//Days Program
+
+	return 0, time.Since(start)
+}
