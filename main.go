f1526b4d28fdbb54861da72105157bd58b5f6f93
Ethan <ethanvolk2424@gmail.com>
2024-12-05 20:45:19 -0600

    Setup For Day 5

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	day02 "AOC24/programs/day02"
 	day03 "AOC24/programs/day03"
 	day04 "AOC24/programs/day04"
+	day05 "AOC24/programs/day05"
 	"fmt"
 	"os"
 	"strconv"
@@ -30,8 +31,10 @@ func main() {
 	day2bans, day2btime := day02.Day2b(m) //Complete
 	day3aans, day3atime := day03.Day3a(m) //Complete
 	day3bans, day3btime := day03.Day3b(m) //Complete
-	day4aans, day4atime := day04.Day4a(m) //Not Done
-	day4bans, day4btime := day04.Day4b(m) //Not Started
+	day4aans, day4atime := day04.Day4a(m) //Complete
+	day4bans, day4btime := day04.Day4b(m) //Complete
+	day5aans, day5atime := day05.Day5a(m) //Complete
+	day5bans, day5btime := day05.Day5b(m) //Complete
 
 	data := [][]string{
 		[]string{"Day01a", strconv.Itoa(day1aans), fmt.Sprintf("%s", day1atime)},
@@ -42,6 +45,8 @@ func main() {
 		[]string{"Day03b", strconv.Itoa(day3bans), fmt.Sprintf("%s", day3btime)},
 		[]string{"Day04a", strconv.Itoa(day4aans), fmt.Sprintf("%s", day4atime)},
 		[]string{"Day04b", strconv.Itoa(day4bans), fmt.Sprintf("%s", day4btime)},
+		[]string{"Day05a", strconv.Itoa(day5aans), fmt.Sprintf("%s", day5atime)},
+		[]string{"Day05b", strconv.Itoa(day5bans), fmt.Sprintf("%s", day5btime)},
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
---
+++ programs/day05/day05.go
@@ -0,0 +1,43 @@
+// /programs/day05.go
+
+package day05
+
+import (
+	"time"
+)
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 05A ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+func Day5a(m int) (int, time.Duration) {
+	start := time.Now()
+	//var lines []string
+	if m == 0 {
+		//lines = util.Returnlines("inputdata/day_05/day_05_test.txt")
+	} else {
+		//lines = util.Returnlines("inputdata/day_05/day_05_actual.txt")
+	}
+
+	//Setup VARS For Today
+	score := 0
+
+	//Days Program
+
+	return score, time.Since(start)
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 05B ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+func Day5b(m int) (int, time.Duration) {
+	start := time.Now()
+	//var lines []string
+	if m == 0 {
+		//lines = util.Returnlines("inputdata/day_05/day_05_test.txt")
+	} else {
+		//lines = util.Returnlines("inputdata/day_05/day_05_actual.txt")
+	}
+
+	//Setup VARS For Today
+	score := 0
+
+	//Days Program
+
+	return score, time.Since(start)
+}
